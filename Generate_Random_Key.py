683e686c5f9bbcd2359bedf18d60d55b94845aae
sketchyboi14 <63756104+sketchyboi14@users.noreply.github.com>
2020-04-15 18:07:29 -0500

    Add files via upload

---
+++ Generate_Random_Key.py
@@ -0,0 +1,15 @@
+from cryptography.fernet import Fernet
+import os
+import time
+
+key = Fernet.generate_key()
+time.sleep(1)
+print(key)
+
+file = open("key.key", "wb")
+file.write(key)
+file.close()
+os.path.abspath("key.key")
+
+time.sleep(1)
+print("\nKey has been created and saved to " + os.path.abspath("key.key") + " (Do not rename key)")
